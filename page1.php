729313ccf0bbadcd67c4a9732011291b3edd24ca
charoenporn <tmon368@gmail.com>
2020-04-13 14:38:10 +0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My secound page PHP script!";
+$x = 15985;
+var_dump(is_int($x));
+
+$x = 3359.85;
+var_dump(is_int($x));
+?>
+
+</body>
+</html>
+</html>
